e285a370c6deefa3109c82a994671b23b872fcb9
Nikolay Chaban <oxidburn@gmail.com>
2013-11-14 01:10:23 +0200

    Initial Commit

---
+++ GoogleMapsSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GoogleMapsSample
+//
+//  Created by Chaban Nikolay on 11/14/13.
+//  Copyright (c) 2013 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GoogleMapsSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GoogleMapsSample
+//
+//  Created by Chaban Nikolay on 11/14/13.
+//  Copyright (c) 2013 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
